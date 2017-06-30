9f420e75149e34611c1072b413147ea6aa346d6b
John Dowd <john.dowd@inspectall.com>
2017-06-30 11:58:45 -0700

    finished logic for js and interface

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -1,15 +1,36 @@
-var ExampleModule = require('./../js/scripts.js').exampleModule;
+var DoctorsModule = require('./../js/scripts.js').doctorsModule;
+
+
+// var displayMovie = function(allMovies) {
+//   for(i = 0; i < allMovies.length; i++) {
+//   $('.show-movies').show().append('<div class="panel">' + '<h3>' + 'Title:  ' + allMovies[i].title + '</h3>' + '<p>' + 'Average Rating:  ' + allMovies[i].vote_average + '</p>' + '<p>' + 'Release Date:  ' + allMovies[i].release_date + '</p>' + '<img src=' + 'https://image.tmdb.org/t/p/w300/' +  allMovies[i].backdrop_path + '>' + '</div>');
+//   }
+// };
+
+var displayDoctors = function(doctors, error) {
+  for (x = 0; x < doctors.data.length; x++) {
+    var curDoctor = doctors.data[x];
+    $('.show-doctors').show().append('<div class="panel"><h3>Name: ' +
+      curDoctor.profile.first_name + ' ' + curDoctor.profile.last_name +
+      '</h3><p>Bio:  ' +
+      curDoctor.profile.bio);
+      // '</p><p>Release Date:  ' +
+      // allMovies[i].release_date +
+      // '</p><img src=https://image.tmdb.org/t/p/w300/' +
+      // allMovies[i].backdrop_path +
+      // '></div>');
+  }
+
+  if(error) { /*do something*/ }
+};
 
 
 $(document).ready(function() {
-
+  var doctorModule = new DoctorsModule();
+  $('#doctor-finder').click(function() {
+    var symptom = $('#symptom').val();
+    doctorModule.getDoctors(symptom, displayDoctors);
+    // empty the current list of doctors
+    $('.show-doctors').empty();
+  });
 });
-exports.getDoctors = function(medicalIssue) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(result) {
-      console.log(result);
-    })
-   .fail(function(error){
-      console.log("fail");
-    });
-};
--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,15 @@
-//business logic stuff for a particular module
-var ExampleModule = function(args) {
-  this.args = args; //to be replaced with constructor arguments
+var apiKey  = require('./../.env').apiKey;
+
+function DoctorsModule() { }
+
+DoctorsModule.prototype.getDoctors = function(medicalIssue, callback) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' +
+    medicalIssue +
+    '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' +
+    apiKey, function(response, error) {
+      callback(response, error);
+    });
+
 };
 
-ExampleModule.prototype.examplePrototype = function() {
-  return 'this is an example prototype method';
-};
-
-exports.exampleModule = ExampleModule;
+exports.doctorsModule = DoctorsModule;
